552363c05669236969d341537b26fd46c4b4b9cd
Justice Gong <justice.gong@hotmail.com>
2017-07-23 21:00:57 -0400

    20170723 Backup

--- 6_Transfer_Key_To_Number.py
+++ 6_Transfer_Key_To_Number.py
@@ -9,40 +9,69 @@ for s in MDR_FOI_LIST:
 	                 error_bad_lines=False)
 print(mdr.shape[0])
 
-key = pd.read_csv(os.path.join(cwd, 'FDA_File', '3_Final_BI_KEY_List.txt'), sep='|', header=0,
+# key = pd.read_csv(os.path.join(cwd, 'FDA_File', '3_Final_BI_KEY_List.txt'), sep='|', header=0,
+#                   encoding='ISO-8859-1', error_bad_lines=False)
+# print(key.shape[0])
+#
+# alcl = pd.read_csv(os.path.join(cwd, 'FDA_File', '5_ALCL.txt'), sep='|', header=0,
+#                   encoding='ISO-8859-1', error_bad_lines=False)
+# print(alcl.shape[0])
+#
+# alcl_bi = pd.read_csv(os.path.join(cwd, 'FDA_File', '5_BI_ALCL_KEY_list.txt'), sep='|', header=0,
+#                   encoding='ISO-8859-1', error_bad_lines=False)
+# print(alcl_bi.shape[0])
+#
+# key_result = pd.merge(key, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
+#          suffixes=('_x', '_y'), copy=True, indicator=False)
+# print(key_result.shape[0])
+# key_result = key_result['REPORT_NUMBER']
+# key_result.to_csv('D:/Workplace/MAUDE/key_result.txt')
+#
+# alcl_result = pd.merge(alcl, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
+#          suffixes=('_x', '_y'), copy=True, indicator=False)
+# print(alcl_result.shape[0])
+# alcl_result = alcl_result['REPORT_NUMBER']
+# alcl_result.to_csv('D:/Workplace/MAUDE/alcl_result.txt')
+#
+# alcl_bi_result = pd.merge(alcl_bi, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
+#          suffixes=('_x', '_y'), copy=True, indicator=False)
+# print(alcl_bi_result.shape[0])
+# alcl_bi_result = alcl_bi_result['REPORT_NUMBER']
+# alcl_bi_result.to_csv('D:/Workplace/MAUDE/alcl_bi_result.txt')
+
+# text = pd.read_csv(os.path.join(cwd, 'FDA_File', '1_Text_BI_List.txt'), sep='|', header=0,
+#                   encoding='ISO-8859-1', error_bad_lines=False)
+# print(text.shape[0])
+#
+# text_result = pd.merge(text, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
+#          suffixes=('_x', '_y'), copy=True, indicator=False)
+# print(text_result.shape[0])
+# text_result = text_result['REPORT_NUMBER']
+# text_result.to_csv('D:/Workplace/MAUDE/text_result.txt')
+
+# FTR = pd.read_csv(os.path.join(cwd, 'FDA_File', '2_FTR_List.txt'), sep='|', header=0,
+#                   encoding='ISO-8859-1', error_bad_lines=False)
+# print(FTR.shape[0])
+#
+# FTR_result = pd.merge(FTR, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
+#          suffixes=('_x', '_y'), copy=True, indicator=False)
+# print(FTR_result.shape[0])
+# FTR_result = FTR_result['REPORT_NUMBER']
+# FTR_result.to_csv('D:/Workplace/MAUDE/FTR_result.txt')
+
+FWM = pd.read_csv(os.path.join(cwd, 'FDA_File', '2_FWM_List.txt'), sep='|', header=0,
                   encoding='ISO-8859-1', error_bad_lines=False)
-print(key.shape[0])
+print(FWM.shape[0])
 
-alcl = pd.read_csv(os.path.join(cwd, 'FDA_File', '5_ALCL.txt'), sep='|', header=0,
-                  encoding='ISO-8859-1', error_bad_lines=False)
-print(alcl.shape[0])
-
-alcl_bi = pd.read_csv(os.path.join(cwd, 'FDA_File', '5_BI_ALCL_KEY_list.txt'), sep='|', header=0,
-                  encoding='ISO-8859-1', error_bad_lines=False)
-print(alcl_bi.shape[0])
-
-key_result = pd.merge(key, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
+FWM_result = pd.merge(FWM, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
          suffixes=('_x', '_y'), copy=True, indicator=False)
-print(key_result.shape[0])
-key_result = key_result['REPORT_NUMBER']
-key_result.to_csv('D:/Workplace/MAUDE/key_result.txt')
-
-alcl_result = pd.merge(alcl, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
-         suffixes=('_x', '_y'), copy=True, indicator=False)
-print(alcl_result.shape[0])
-alcl_result = alcl_result['REPORT_NUMBER']
-alcl_result.to_csv('D:/Workplace/MAUDE/alcl_result.txt')
-
-alcl_bi_result = pd.merge(alcl_bi, mdr, how='inner', on='MDR_REPORT_KEY', left_index = True, right_index=False, sort=True,
-         suffixes=('_x', '_y'), copy=True, indicator=False)
-print(alcl_bi_result.shape[0])
-alcl_bi_result = alcl_bi_result['REPORT_NUMBER']
-alcl_bi_result.to_csv('D:/Workplace/MAUDE/alcl_bi_result.txt')
-
+print(FWM_result.shape[0])
+FWM_result = FWM_result['REPORT_NUMBER']
+FWM_result.to_csv('D:/Workplace/MAUDE/FWM_result.txt')
 
 #String manipulation
-with open('alcl_bi_result.txt', 'r', encoding='utf-8') as r:
-	with open('3_ALCL_BI_Number_List.txt', 'w', encoding='utf-8') as w:
+with open('FWM_result.txt', 'r', encoding='utf-8') as r:
+	with open('3_FWM_result.txt.txt', 'w', encoding='utf-8') as w:
 		for line in r:
 			line = line.split(',')
 			w.write(line[1])
