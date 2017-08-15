50075655b0a11a20d0bcadf45fc3370f74dec512
Justice Gong <justice.gong@hotmail.com>
2017-08-15 13:01:47 -0400

    Updated manufacturer full value list

--- 10_Column_Value_Count.py
+++ 10_Column_Value_Count.py
@@ -4,7 +4,12 @@ import pandas as pd
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 
-df_FOI = pd.read_csv('D:/Workplace/MAUDE/Data/MDRFOI_BI_FULL_TABLE.txt', sep='|', header=0, encoding='ISO-8859-1',
+# df_FOI = pd.read_csv('D:/Workplace/MAUDE/Data/MDRFOI_BI_FULL_TABLE.txt', sep='|', header=0, encoding='ISO-8859-1',
+#                      error_bad_lines=False)
+# a = df_FOI['SOURCE_TYPE'].value_counts()
+# a.to_csv('mdrfoi_SOURCE_TYPE_full_list.csv')
+
+df_FOI = pd.read_csv('D:/Workplace/MAUDE/WF/DEV_BI_FULL_TABLE.txt', sep='|', header=0, encoding='ISO-8859-1',
                      error_bad_lines=False)
-a = df_FOI['SOURCE_TYPE'].value_counts()
-a.to_csv('mdrfoi_SOURCE_TYPE_full_list.csv')
+a = df_FOI['MANUFACTURER_D_NAME'].value_counts()
+a.to_csv('foidev_manufacture_name_full_list.csv')
