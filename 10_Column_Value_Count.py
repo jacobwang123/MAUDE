036846969c0079071e9cf7833710e094d53dee5c
Justice Gong <justice.gong@hotmail.com>
2017-08-22 14:01:59 -0400

    Weekly Report Draft 2 + word cloud

--- 10_Column_Value_Count.py
+++ 10_Column_Value_Count.py
@@ -11,5 +11,4 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 
 df_FOI = pd.read_csv('D:/Workplace/MAUDE/WF/DEV_BI_FULL_TABLE.txt', sep='|', header=0, encoding='ISO-8859-1',
                      error_bad_lines=False)
-a = df_FOI['MANUFACTURER_D_NAME'].value_counts()
-a.to_csv('foidev_manufacture_name_full_list.csv')
+a.to_csv('D:/Workplace/MAUDE/FDA_0824_Weekly_Report/foidev_brand_name_full_list.csv')
--- WF/whole_text_optimizer.py
+++ WF/whole_text_optimizer.py
@@ -1,13 +1,13 @@
 # website for wordcloud: http://www.wordclouds.com/
 import math
-with open('Word_Cloud.txt', 'r') as rf:
-	with open('Word_Cloud_Optimized_Text.txt', 'w') as wf:
+with open('website_WF.txt', 'r') as rf:
+	with open('website_WF_Optimized_Text.txt', 'w') as wf:
 		m = 0
 		for line in rf:
 			line = line.replace('\n', '')
 			line = line.split('\t')
-			line[0] = int(math.log(int(line[0]), 2))
+			line[0] = int(math.log(int(line[0]), 1.2))
 			if line[0] >= 5:
-				wf.write(str(line[0] * 4) + '\t' + str(line[1] + '\n'))
+				wf.write(str(line[0] * 2) + '\t' + str(line[1] + '\n'))
 		wf.close()
 	rf.close()
